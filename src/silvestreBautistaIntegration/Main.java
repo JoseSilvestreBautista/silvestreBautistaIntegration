2dd8e3b2f9dffa1863918044413efa73088168e6
Jose Silvestre-Bautista <43448715+JoseSilvestreBautista@users.noreply.github.com>
2019-04-13 15:39:53 -0400

    Update Main.java

--- src/silvestreBautistaIntegration/Main.java
+++ src/silvestreBautistaIntegration/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
 // Hi, I am Jose Silvestre-Bautista ctrl+shift+f for formatting
 // This program is filled with exercises I have completed and it serves as a reference.
@@ -8,12 +10,91 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-    System.out.println("Hello, I am a program.");
+    System.out.println("Hi, this porgram is hogwash. It is my reference for coding, enjoy.");
+    System.out.println("A litle about me and my family:");
+    Vehicle momsCar = new Car("CTS", "Cadillac", 2016, 160);
+    Car myCar = new Car("Ford", "SE", "Apollo");
+    myCar.printCarStats();
+    ((Car) momsCar).momCar();
+
+    System.out
+        .println("As part of my program I have a college credit title classifier and it have four "
+            + "credit year names in it:");
+
+    YearClassification studentCredits = new YearClassification();
+    String[] studentYear = {"Freshman", "Sophomre", "Junior", "Senior"};
+    for (String yearNum : studentYear) {
+      System.out.println(yearNum);
+    }
+
+    System.out.println("Enter how many credits you have to see your classification.");
+    System.out
+        .println("By the way, if you have no credits you are automactically " + "clssified as a ");
+    studentCredits.print();
+
+    int exceptionStopper = 1;
+    do {
+      try {
+        int usersCreditAmount = scan.nextInt();
+        studentCredits.setCredits(usersCreditAmount);
+        if (usersCreditAmount < 0 || usersCreditAmount > 160) {
+          throw new InputMismatchException();
+        }
+        studentCredits.print();
+        exceptionStopper = 2;
+      } catch (Exception ex) {
+        System.out.println("Please, enter a whole number.");
+        scan.next();
+      }
+
+    } while (exceptionStopper == 1);
+
+    int[] creditClasses = {2, 15, 43, 19, 4};
+    int minCredit = creditClasses[0];
+    int minIndex = 0;
+    for (int index = 1; index < creditClasses.length; index++) {
+      if (creditClasses[index] < minCredit) {
+        minCredit = creditClasses[index];
+        minIndex = index;
+      }
+    }
+    System.out.println(
+        "Anyway, among my friends who have completed 2, 15, 43, 19, " + "and 4 credit hours.");
+    System.out.println("The minCredit hour between them is " + minCredit);
+    System.out.println("In this program code, the minCredit hour is located in element " + minIndex
+        + "" + "of the array it is stored.");
+
+    int creditSum = 0;
+    for (int counter = 0; counter < creditClasses.length; counter++) {
+      creditSum += creditClasses[counter];
+    }
+    System.out.println(
+        "The total credits completed by all my friends is " + creditSum+".");
+
+System.out.println("At lunch I set with:");
+
+    String[][] lunchSitting = {{"Tom", "Jim"}, {"Lin", "Jose"}};
+    for (int rowSeat = 0; rowSeat < lunchSitting.length; rowSeat++) {
+      for (int columnsSeat = 0; columnsSeat < lunchSitting.length; columnsSeat++) {
+        System.out.println("lunchSitting[" + rowSeat + "][" + columnsSeat + "]="
+            + lunchSitting[rowSeat][columnsSeat]);
+      }
+    }
+
+    System.out.println("By the way, at school I take: ");
+    ArrayList<String> courses = new ArrayList<String>();
+    courses.add("Calculus III");
+    courses.add("Intermediate Mechanics");
+    courses.add("Composition II");
+    courses.add("Statistical Methods");
+    courses.add("Intro to Programming");
+    System.out.println(courses);
+    
     System.out.println("Please, allow me to get to know you.");
     System.out.println("What is your name?");
 
-    Scanner scan = new Scanner(System.in);
     final String name = scan.next();
     // final locks in the value of a variable, but a reference to it can be
     // modified
@@ -31,7 +112,7 @@ public class Main {
       System.out.println("Try again");
       scan.nextBoolean();
       if (true)
-        break; // ends the loop when break is read, this prevents an infinte loop from occuring.
+        break;
     }
 
     // Casting: forcing java to treat a variable as another data type
@@ -42,7 +123,6 @@ public class Main {
 
     int grade = 90;
     char finalLetter = (grade >= 90) ? 'A' : 'B';
-    //////////////////////////////////////////////////////////////////////////////////////////////
     if (grade >= 90) {
       finalLetter = 'A';
     } else if (grade >= 80) {
@@ -53,7 +133,6 @@ public class Main {
       finalLetter = 'F';
     }
     System.out.println(finalLetter);
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     Random randomGen = new Random();
     // repl.it.com
     randomGen.nextInt(54);
@@ -70,7 +149,6 @@ public class Main {
     double pricePerInch = calculatePricePerInch(pizzaDiameter, pizzaPrice);
     // output the price per inch
     System.out.println("Pizza price per inch = " + pricePerInch);
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     int day = 3;
     String dayOfWeek;
     switch (day) {
@@ -100,14 +178,12 @@ public class Main {
         break;
     }
     System.out.println(dayOfWeek);
-    //////////////////////////////////////////////////////////////////////////////////////////
     String blurb = "abc";
     String blurb2 = "def";
 
     System.out.println(blurb.compareTo(blurb2));
     System.out.println(blurb == blurb2);
     // == checks if both objects point to the same memory location
-    ///////////////////////////////////////////////////////////////////////////////////////
 
     int randomNumberPicked;
     for (randomNumberPicked = 5; randomNumberPicked >= 0; randomNumberPicked--) {
@@ -129,7 +205,7 @@ public class Main {
       System.out.println(count);
       count++;
     } while (count <= 10);
-    // evaluates statement before the condition, even if condition is not meet
+    // evaluates statement before the condition even if condition is not meet
 
     System.out.println("enter a number between 1 and 100");
     int N = scan.nextInt();
@@ -157,21 +233,18 @@ public class Main {
     int userNum5 = scan.nextInt();
     userNum5 += 1;
     System.out.println("your number plus 1 is " + userNum5);
-
-
-
   }
 
-
-
   public static double calculatePricePerInch(double pizzaDiameter, double pizzaPrice) {
-    // the entire line 77 is a header
+    // the entire line above is a header
     // inside the parenthesis of calculatePricePerInch is the parameter
     return pizzaPrice / (Math.PI * (pizzaDiameter / 2) * (pizzaDiameter / 2));
   }
-
-
 }
+
+// inheritance is a relation between two classes: super and sub class
+// inheritance allows a sub class to inherit the fields and methods of a super class
+// This reduces coding time and increases organization.
 
 // operator precedence: certain operators are evaluated first because they have higher importance
 // example: "=" has higher importance than "()"
@@ -202,4 +275,3 @@ public class Main {
 
 // char: The char data type is a single 16-bit Unicode character. It has a minimum value of 0 and a
 // maximum value of 65,535 inclusive.
-
