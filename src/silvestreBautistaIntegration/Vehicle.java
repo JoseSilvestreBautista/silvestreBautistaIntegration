32169bd372bae5025e35132c693a31188021d5fe
Jose Silvestre-Bautista <43448715+JoseSilvestreBautista@users.noreply.github.com>
2019-04-13 15:42:45 -0400

    Create Vehicle.java

---
+++ src/silvestreBautistaIntegration/Vehicle.java
@@ -0,0 +1,24 @@
+
+public class Vehicle {
+  private int wheels;
+  private int topSpeed;
+  private String vehicleName;
+
+  public void setVehicleName(String vehicleName) {
+    this.vehicleName = vehicleName;
+  }
+
+  public String getVehicleName() {
+    return vehicleName;
+  }
+
+  public void setTopSpeed(int topSpeed) {
+    this.topSpeed = topSpeed;
+  }
+
+  public int getTopSpeed() {
+    return topSpeed;
+  }
+
+
+}
